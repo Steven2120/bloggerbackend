004ee64eb384cd2120cf21ecab57c93e00841dae
Steven Moyano <stevenmoyano1@gmail.com>
2022-07-07 17:21:47 -0500

    third commit

--- routes/blogs.js
+++ routes/blogs.js
@@ -7,10 +7,35 @@ router.get("/hello-blogs", (req, res) => {
 });
 
 router.get("/all-blogs", async (req, res) => {
+  const limit = Number(req.query.limit);
+  const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
+  const sortField = req.query.sortField;
+  // if sort field is equal to "ASC", then sequential (1), if not, sequential in reverse order (-1).
+  const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+  const filterField = req.query.filterField;
+  const filterValue = req.query.filterValue;
+
   try {
     const collection = await blogsDB().collection("blogs50");
-    const posts = await collection.find({}).toArray();
-    res.send({ message: posts });
+
+    let filterObj = {};
+    if (filterField && filterValue) {
+      filterObj = { [filterField]: filterValue };
+    }
+    let sortObj = {};
+    if (sortField && sortOrder) {
+      sortObj = { [sortField]: sortOrder };
+    }
+
+    // const posts = await collection.find({}).toArray();
+    const posts = await collection
+      .find(filterObj)
+      .sort(sortObj)
+      .limit(limit)
+      .skip(skip)
+      .toArray();
+
+    res.json({ message: posts });
   } catch (error) {
     res.status(500).send("error fetching posts " + error);
   }
